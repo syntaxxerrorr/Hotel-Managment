65589e75d83206fe6d275ea29ad0fe5ce057fc8e
syntaxxerrorr <fahad@Fahads-MacBook-Pro.local>
2015-03-07 20:17:03 +0500

    Initial Commit

---
+++ Hotel Mangment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hotel Mangment
+//
+//  Created by Fahad Mustafa on 07/03/2015.
+//  Copyright (c) 2015 FahadMusafa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
